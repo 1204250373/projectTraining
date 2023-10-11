2190d8d83f5f6a9c762ad072ce24f610338c6689
xiaoju007 <1816732933@qq.com>
2023-10-11 11:37:50 +0800

    myuser表和User类

--- myuser.sql
+++ myuser.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 80032
 File Encoding         : 65001
 
-Date: 2023-10-11 10:49:14
+Date: 2023-10-11 11:31:15
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,15 +20,19 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `myuser`;
 CREATE TABLE `myuser` (
-  `uid` int NOT NULL,
-  `phone` varchar(11) COLLATE utf8mb3_unicode_520_ci NOT NULL,
-  `sid` varchar(10) COLLATE utf8mb3_unicode_520_ci NOT NULL,
+  `uid` int NOT NULL AUTO_INCREMENT,
+  `sid` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_520_ci NOT NULL,
+  `phone` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_520_ci DEFAULT NULL,
+  `password` varchar(16) COLLATE utf8mb3_unicode_520_ci NOT NULL,
   `userType` enum('用户','管理员','商家') COLLATE utf8mb3_unicode_520_ci NOT NULL,
-  `userName` varchar(20) COLLATE utf8mb3_unicode_520_ci NOT NULL,
-  `userBalance` int NOT NULL,
-  PRIMARY KEY (`uid`,`phone`,`sid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_520_ci;
+  `userBalance` double NOT NULL,
+  PRIMARY KEY (`uid`,`sid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_520_ci;
 
 -- ----------------------------
 -- Records of myuser
 -- ----------------------------
+INSERT INTO `myuser` VALUES ('1', '2240129516', '17602024903', '123456', '用户', '0');
+INSERT INTO `myuser` VALUES ('2', '1010101010', null, '123456', '用户', '0');
+INSERT INTO `myuser` VALUES ('3', '2240129107', '110', '123456', '管理员', '0');
+INSERT INTO `myuser` VALUES ('4', '2240129623', '120', '123456', '管理员', '0');
